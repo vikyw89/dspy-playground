f627529dcd60de6f61099b08dff13e524d82b82b
vikyw89 <vikyw89@gmail.com>
2024-05-11 01:48:00 +0800

    feat: init

---
+++ src/main.py
@@ -0,0 +1,14 @@
+
+
+async def main():
+    # import src.examples.initial
+    pass
+    import src.examples.rag
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
